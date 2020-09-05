a371b7c03fa6b1a2d0395fa3818c769ebdf55afb
socsieng <socsieng@gmail.com>
2020-09-05 20:32:05 +0000

    fix: update script to work with gitpod.io

--- scripts/update-modified-date.sh
+++ scripts/update-modified-date.sh
@@ -4,12 +4,12 @@ file_path=$1
 timestamp=$2
 date_exp='[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9] [+-][0-9][0-9][0-9][0-9]'
 
-modified_match=`cat $file_path | grep -E "^modified_date:\\s+$date_exp"`
+modified_match=`cat $file_path | grep -E "^modified_date: *$date_exp"`
 
 if [ -n "$modified_match" ]
 then
-  sed -E -e "s/^modified_date: *$date_exp/modified_date: $timestamp/" -i "" $file_path
+  sed -i -E -e "s/^modified_date: *$date_exp/modified_date: $timestamp/" $file_path
 else
-  sed -El -e "s/^(date: *$date_exp)/\\1\\
-modified_date: $timestamp/" -i "" $file_path
+  sed -i -E -e "s/^(date: *$date_exp)/\\1\\
+modified_date: $timestamp/" $file_path
 fi
